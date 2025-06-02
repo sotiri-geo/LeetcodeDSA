48ec433198150feb03121a3521ab7b2ad6501e24
Sotiri <44702437+Tiri1992@users.noreply.github.com>
2025-06-02 15:00:02 +0100

    Time: 163 ms (13.26%), Space: 19.2 MB (21.45%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,52 +1,46 @@
-from collections import Counter
+from collections import Counter, deque
 import heapq
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         """
-        Suppose that we look at the time till we can next process a char.
-        We need to manage the following state variables:
+        Each interval can either be idle or allow completion of a single task. 
+        i.e. no parallel workstreams.
 
-        Frequency of tasks A-Z
-        Some way to find out how many intervals has passed since the last task
-        has been processed and see if we can select it.
-
-        We can use that as a means of tracing back when it was last run. We assume at time
-        t we push every character with their frequency on a heap.
-
-        We can sort heap based off time interval which we track t = 1, t = 2 etc
-        then we decrement the count of the task and only add back to the heap if count > 0.
-
-        This is a greedy approach because we don't specifically choose a character based on any other
-        property but its count. If multiple characters can be selected we just pick the first in heap.
+        Constraint: gap of at least n intervals between two tasks with same label
+        so
+        A _ _ A _ _ A
         """
 
-        
+        # greedy and pick the highest frequency
         counter = Counter(tasks)
+        # consider only the frequency on a max heap, we don't care about task name
+        heap = [-val for val in counter.values()]
 
-        # heap needs to track time since last run 
-        heap = [(0, count, task) for task, count in counter.items()]
-
-        # Linear time
         heapq.heapify(heap)
 
+        # (time_ready, frequency) -> time_ready = time + n + 1 (i.e next time we can 
+        # use it)
+        cooldown_q = deque()
+
         time = 0
-        # Could we add the next time its allowed to run i.e. curr_time + n
-        # We track time independently, so when time >= time_next_run we can process this
 
-        while heap:
-            time_next_run, count, task = heapq.heappop(heap)
-
-            # process and decrement by 1
-            if time >= time_next_run:
-                if count - 1 > 0:
-                    heapq.heappush(heap, (time_next_run + n + 1, count - 1, task))
-            else:
-                # ignore and let time elapse
-                heapq.heappush(heap, (time_next_run, count, task))
-            
+        while heap or cooldown_q:
+            # check cooldown queue to see if we can add any back to heap
+            if cooldown_q and cooldown_q[0][0] <= time:
+                t, val = cooldown_q.popleft()
+                heapq.heappush(heap, val)
+            if heap:
+                val = heapq.heappop(heap)
+                if -val > 1:
+                    # need to cooldown
+                    cooldown_q.append((time + n + 1, val + 1))
             time += 1
 
-        # Once all tasks have been exhausted in heap then return time
         return time
+
+
+
+            
+
 
