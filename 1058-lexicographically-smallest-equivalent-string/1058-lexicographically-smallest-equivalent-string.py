d3fa93e18d74b29e88e0989098cf646851061af6
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-06-29 11:57:04 +0100

    Time: 0 ms (100%), Space: 17.7 MB (97.82%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
@@ -5,7 +5,6 @@ class UnionFind:
     """Path compression and union by rank"""
     def __init__(self, size):
         self.parent = [i for i in range(size)]
-        self.rank = [1] * size
 
     def find(self, x):
         if x != self.parent[x]:
@@ -13,19 +12,19 @@ class UnionFind:
         return self.parent[x]
 
     def union(self, x, y):
+        """Keep track of the smallest element. This positions the root to 
+        be the smallest lexographic element.
+        """
         root_x, root_y = self.find(x), self.find(y)
 
         if root_x == root_y:
             # Already connected
             return
 
-        if self.rank[root_x] > self.rank[root_y]:
+        if root_x < root_y:
             self.parent[root_y] = root_x
-        elif self.rank[root_x] < self.rank[root_y]:
-            self.parent[root_x] = root_y
         else:
-            self.parent[root_y] = root_x
-            self.rank[root_y] += 1
+            self.parent[root_x] = root_y
     
 
 
@@ -40,28 +39,16 @@ class Solution:
         alpha = "abcdefghijklmnopqrstuvwxyz"
         uf = UnionFind(len(alpha))
 
-        alpha_map = {s: idx for idx, s in enumerate(alpha)}
-
         for i in range(len(s1)):
-            # connect the nodes to the same parent root
-            char1_idx, char2_idx = alpha_map[s1[i]], alpha_map[s2[i]]
+            # Relative positions
+            x = ord(s1[i]) - ord('a')
+            y = ord(s2[i]) - ord('a')
 
-            uf.union(char1_idx, char2_idx)
-        
-        # We now need to sort by each component and get the smallest value
-        # Use a heap to manage smallest lexographic element
-        root_to_min_heap = defaultdict(list)
-        for i in range(len(alpha)):
-            char = alpha[i]
-            root = uf.find(i)
-            heapq.heappush(root_to_min_heap[root], char)
+            uf.union(x, y)
 
-        ans = []
+        # now each node in the union find points to the smallest lexographic
+        # value the given nodes connected component
 
-        for c in baseStr:
-            root_idx = uf.find(alpha_map[c])
-            # First element is smallest
-            ans.append(root_to_min_heap[root_idx][0])
-        
+        ans = [alpha[uf.find(ord(c) - ord('a'))] for c in baseStr]
+
         return "".join(ans)
-        
