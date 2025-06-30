e35bc1882521e020fc62fcca6a64aa2a119b0319
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-06-30 15:32:44 +0100

    Time: 119 ms (88.46%), Space: 35.5 MB (84.62%) - LeetHub

--- 3088-maximal-range-that-each-element-is-maximum-in-it/3088-maximal-range-that-each-element-is-maximum-in-it.py
+++ 3088-maximal-range-that-each-element-is-maximum-in-it/3088-maximal-range-that-each-element-is-maximum-in-it.py
@@ -1,5 +1,30 @@
 class Solution:
     def maximumLengthOfRanges(self, nums: List[int]) -> List[int]:
+        """
+        Default to each element being a max in at a subarray containing itself.
+        Then we can decide to update based of mono stack.
+
+        If we construct a decreasing mono stack we know the largest value we've seen
+        is at the bottom of stack.
+
+        The trick is to calculate the range for each element that gets popped off.
+
+        As we pop an element j off the stack if the stack is non empty, the top of stack 
+        should contain a value nums[stack[-1]] > nums[j] (as its monotonic decreasing) and we know 
+        the value nums[i] is greater than nums[j] as well. So here we found its bound for the
+        subarray for which nums[j] is maximal.
+
+        nums[stack[-1]] > nums[j]
+        nums[i] > nums[j]
+
+        bounded to left = stack[-1]
+        bounded to right = i
+
+        subarray size = i - stack[-1] - 1
+
+        Edge case is when there is no element left on the stack after we pop, hence its maximal
+        up to i.
+        """
         res = [1]*len(nums)
         nums.append(float('inf'))
         stack = []
