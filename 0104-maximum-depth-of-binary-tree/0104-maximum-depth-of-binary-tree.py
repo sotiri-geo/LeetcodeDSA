77ee3eb9f5bf2b7185aa7583d8d0aa77acb34fe9
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-08-07 13:19:57 +0100

    Time: 2 ms (36.93%), Space: 19 MB (78.48%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        """We can identify a leaf node if it had null children"""
+        ans = 0
+
+        def dfs(node, depth):
+            nonlocal ans
+            if not node:
+                ans = max(ans, depth)
+                return 
+
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+
+        dfs(root, 0)
+        return ans
