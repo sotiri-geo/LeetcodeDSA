5bb7b0f1f7eb87c5c3f5a5acd0fdfd705de8ce93
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-09-02 09:43:23 +0100

    Time: 19 ms (37.29%), Space: 19.9 MB (74.6%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,24 +1,23 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        # Lets try do a bottom up solution
+        # for a given grid[i][j] we could have come from above or to the left
+        # then we need to work out the min cost of coming from the left or above
+        # we can mutate just the grid because after we've visited (i, j) we can
+        # guarentee we've come from a min
+
         row_size = len(grid)
         col_size = len(grid[0])
-        def is_valid(row, col):
-            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
 
-        @cache
-        def dp(row, col):
-            if row == row_size - 1 and col == col_size - 1:
-                # bottom right
-                return grid[row][col]
-            
-            # out of bounds
-            if not is_valid(row, col):
-                return float('inf')
-
-            
-            right = dp(row, col + 1) + grid[row][col]
-            down = dp(row + 1, col) + grid[row][col]
-
-            return min(right, down)
-
-        return dp(0, 0)
+        for i in range(row_size):
+            for j in range(col_size):
+                curr_cost = grid[i][j]
+                if i == 0 and j == 0:
+                    # base case
+                    continue
+                above = grid[i - 1][j] if i > 0 else float('inf')
+                left = grid[i][j - 1] if j > 0 else float('inf')
+                # record the min cost to arrive at this point + cost of point itself
+                grid[i][j] = min(above + curr_cost, left + curr_cost)
+        
+        return grid[row_size - 1][col_size - 1]
