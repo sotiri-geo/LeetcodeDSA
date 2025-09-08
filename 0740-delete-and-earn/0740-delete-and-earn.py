8736eba6e2ca87ed9bc6d6b05ee45f22e93daf98
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-09-08 08:16:23 +0100

    Time: 7 ms (79.34%), Space: 19.7 MB (56.22%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,18 +1,22 @@
 from collections import Counter
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-
         # sort points for points[v] = v * count[v]
         points = [0] * (max(nums) + 1)
         counter = Counter(nums)
         # fill in points
         for k, v in counter.items():
             points[k] = k * v
+        # edge cases
+        if not nums:
+            return 0
+        if len(points) == 1:
+            return points[0]
 
         # Now similar to house robber problem
         dp = [0] * len(points)
         dp[0] = points[0]
-        dp[1] = points[1]
+        dp[1] = max(points[0], points[1]) # better of the two
         
         for i in range(2, len(points)):
             # recurrence relation
