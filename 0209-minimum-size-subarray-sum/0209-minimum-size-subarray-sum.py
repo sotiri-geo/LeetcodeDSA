80516a9cf1e15c5fa83c04b3b2b1c4d4b086a7e5
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-10-04 17:48:46 +0100

    Time: 14 ms (24.23%), Space: 28.3 MB (80.53%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,15 +1,24 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        
-        left = 0
-        ans = float('inf')
-        curr = 0
+        """
+        Two pointer to track both the size of the array and whether the constraint is fulfuilled.
+        In the event the constraint is broken, we need to fix it within inner loop.
+        """
+        left = right = 0 
+        min_length = float('inf')
+        curr_sum = 0
+
         for right in range(len(nums)):
-            curr += nums[right]
-            while curr >= target:
-                ans = min(right - left + 1, ans)
-                curr -= nums[left]
+            curr_sum += nums[right]
+
+            while left <= right and curr_sum >= target:
+                # move left pointer forward to minimise subarray size
+                # Always a valid constraint in this block as
+                # curr_sum >= target
+                min_length = min(min_length, right - left + 1)
+                # remove from left
+                curr_sum -= nums[left]
                 left += 1
         
-        return ans if ans != float('inf') else 0
-         
+        return min_length if min_length != float('inf') else 0
+            
