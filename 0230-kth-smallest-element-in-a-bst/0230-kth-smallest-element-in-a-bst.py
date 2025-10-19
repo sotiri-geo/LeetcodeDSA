407cbd41aa208fd4e2e9f0ced62c46d6adb4cc3d
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-10-19 08:16:53 +0100

    Time: 0 ms (100%), Space: 21.2 MB (27.73%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        """
+        Time complexity is O(N) and space is O(N)
+        """
+        nums = []
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            nums.append(node.val)
+            inorder(node.right)
+        
+        inorder(root)
+        # print(nums)
+        return nums[k - 1]
