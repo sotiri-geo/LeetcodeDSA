9e48c9eab45a2d4469ad9f94cd8e3ad5016e7c6d
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-11-19 21:46:51 +0000

    Time: 3 ms (57.55%), Space: 9.4 MB (17.95%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.go
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.go
@@ -13,14 +13,22 @@ func reverseWords(s string) string {
     var ans strings.Builder
 
     words := strings.Fields(s)
+    ans.Grow(len(s)) // Avoids realloc 
 
-    for i, subString := range words {
-        ans.WriteString(reverse(subString))
-        if i == len(words) - 1 {
-            break
+    for i, word := range words {
+        // Add a space before each word instead of the first
+        if i > 0 {
+            ans.WriteByte(' ')
         }
-        ans.WriteString(" ")
-    }
-    
+
+        // In place swapping is more memory efficient than building a new String    
+        // Using two pointers to reverese the strings
+        runes := []rune(word)   
+        for left, right := 0, len(runes) - 1; left < right; left, right = left + 1, right - 1 {
+            runes[left], runes[right] = runes[right], runes[left]
+        }
+        ans.WriteString(string(runes))
+    }    
+
     return ans.String()
 }
