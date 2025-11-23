21607762e63057a7ed4804a76a9a5275a2b27096
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-11-23 12:10:28 +0000

    Time: 0 ms (100%), Space: 12.5 MB (20.93%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.go
+++ 2540-minimum-common-value/2540-minimum-common-value.go
@@ -1,22 +1,18 @@
+// getCommon returns the min integer value common in both nums1 and nums2
+// else returns -1 if no common integer exits
 func getCommon(nums1 []int, nums2 []int) int {
-    // Define a pointer at the start of both slices
     p1, p2 := 0, 0
 
-    // We need to traverse both slices from the start
     for (p1 < len(nums1)) && (p2 < len(nums2)) {
         if nums1[p1] == nums2[p2] {
-            // We have found min int common it both so return
             return nums1[p1]
         }
         if nums1[p1] < nums2[p2] {
-            // inc p1   
             p1++
-            continue
+        } else {
+            p2++
         }
-        // Else we need to increment p2
-        p2++
     }
 
-    // If we cannot find a common min int in both arrays
     return -1
 }
