4bb44e7165516e7094f71998fa13de8575cdd1f3
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-11-23 12:11:22 +0000

    Time: 0 ms (100%), Space: 10.8 MB (77.91%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.go
+++ 2540-minimum-common-value/2540-minimum-common-value.go
@@ -3,7 +3,7 @@
 func getCommon(nums1 []int, nums2 []int) int {
     p1, p2 := 0, 0
 
-    for (p1 < len(nums1)) && (p2 < len(nums2)) {
+    for p1 < len(nums1) && p2 < len(nums2) {
         if nums1[p1] == nums2[p2] {
             return nums1[p1]
         }
