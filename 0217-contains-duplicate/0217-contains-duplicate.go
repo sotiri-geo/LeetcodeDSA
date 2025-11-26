f1b002acb1fa0e12eed667944026af841baa13fb
Sotiri <44702437+sotiri-geo@users.noreply.github.com>
2025-11-26 20:04:35 +0000

    Time: 32 ms (15.04%), Space: 12.3 MB (39.53%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -0,0 +1,11 @@
+func containsDuplicate(nums []int) bool {
+    seen := map[int]struct{}{}
+
+    for _, num := range nums {
+        if _, exists := seen[num]; exists {
+            return true
+        }
+        seen[num] = struct{}{}
+    }
+    return false
+}
