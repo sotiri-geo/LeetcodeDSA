bf7d52137a69961bbfb879ac1ac834309f747050
Sotiri <sotiri@thoughtmachine.net>
2026-02-21 10:49:29 +0000

    Time: 296 ms (8.44%), Space: 31.7 MB (6.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,39 +1,51 @@
-from collections import deque
+from collections import defaultdict
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         """
-        Implement a bfs algo
-        """
-        
-        # explore each component
+        We look at the islands as connected components with 
+        each section of the grid == 1 as a node.
 
-        islands = 0
-        queue = deque()
-        seen = set()
-        m, n = len(grid), len(grid[0])
+        Returning the no. of islands is equivalent to returning
+        the number of connected components.
+
+        The input is not an adj matrix, we need to traverse the 
+        grid, we start at a given point, and if its '1' then we 
+        can then look left, right, up, down
+        """
+
+        graph = defaultdict(list)
+        n, m = len(grid), len(grid[0])
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         def is_valid(x, y):
-            return 0 <= x < m and 0 <= y < n and grid[x][y] == "1"
+            """Returns true if x, y within boundaries and is land"""
+            return 0 <= x < n and 0 <= y < m and grid[x][y] == "1"
 
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        # explore component
+        def dfs(node):
 
-        def bfs(x, y):
-            queue = deque()
-            queue.append((x, y))
-            
-            while queue:
-                currx, curry = queue.popleft()
-                for dx, dy in directions:
-                    new_pnt = (dx + currx, dy + curry)
-                    if new_pnt not in seen and is_valid(new_pnt[0], new_pnt[1]):
-                        seen.add(new_pnt)
-                        queue.append(new_pnt)
-
-            return 1
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1" and (i, j) not in seen:
-                    islands += bfs(i, j)
+            for dx, dy in directions:
+                nei = node[0] + dx, node[1] + dy
+                if is_valid(nei[0], nei[1]) and nei not in seen:
+                    seen.add(nei)
+                    dfs(nei)
+        
+        seen = set()
+        islands = 0
+        # O(N*M) time complexity
+        for i in range(n):
+            for j in range(m):
+                if is_valid(i, j) and (i, j) not in seen:
+                    islands += 1
+                    seen.add((i, j))
+                    dfs((i, j))
 
         return islands
+
+
+
+        
+                    
+        
